31a7ffc0b6961208613b0ad0401825f834995dc4
Carlos Diaz <carlos12one@hotmail.com>
2023-06-02 11:05:56 -0500

    get user

--- tiktok-server/users/api/router.py
+++ tiktok-server/users/api/router.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from users.api.views import RegisterView, UserMeView
+from rest_framework.routers import DefaultRouter
+from users.api.views import RegisterView, UserMeView, UserApiViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+
+router_user = DefaultRouter()
+
+router_user.register(prefix='users', basename='users', viewset=UserApiViewSet)
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view()),
